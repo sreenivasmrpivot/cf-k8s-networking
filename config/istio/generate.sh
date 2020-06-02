c11fcbb7ba74a0fc696fb51b6fff185f79bf0acc
Alex Standke <astandke@pivotal.io>
2020-06-02 19:30:52 +0000

    add our envoy image and jaeger

--- config/istio/generate.sh
+++ config/istio/generate.sh
@@ -14,9 +14,13 @@ fi
 echo "generating Istio resource definitions to ${SCRIPT_DIR}/tmp/generated-istio.yaml ..." >&2
 mkdir -p "${SCRIPT_DIR}/../../istio"
 mkdir -p "${SCRIPT_DIR}/tmp"
-istioctl manifest generate -f "${SCRIPT_DIR}/istio-values-gen.yaml" "$@" > "${SCRIPT_DIR}/tmp/generated-istio.yaml"
+istioctl manifest generate -f <(ytt -f ${SCRIPT_DIR}/istio-values.yaml) "$@" > "${SCRIPT_DIR}/tmp/generated-istio.yaml"
 
 ytt --ignore-unknown-comments \
   -f "${SCRIPT_DIR}/ytt-data-values.yaml" \
   -f "${SCRIPT_DIR}/tmp/generated-istio.yaml" \
-  -f "${SCRIPT_DIR}/overlays"
+  -f "${SCRIPT_DIR}/overlays/ingressgateway-daemonset.yaml" \
+  -f "${SCRIPT_DIR}/overlays/kapp-compatible.yaml" \
+  -f "${SCRIPT_DIR}/overlays/mtls-peer-authentication.yaml" \
+  -f "${SCRIPT_DIR}/overlays/sidecar-resource.yaml"
+
